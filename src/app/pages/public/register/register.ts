edecb7a0a5c2fc4ed530f9e6de7c5181c6bf6ae2
ke-ps <perezkevin166.kp@gmail.com>
2026-06-05 13:55:35 +0200

    Remove profile creation call from registration process

--- src/app/pages/public/register/register.ts
+++ src/app/pages/public/register/register.ts
@@ -59,7 +59,6 @@ export class Register {
     try {
       const { email, password, name, surname, birth_date, notifications } = this.form.getRawValue();
       await this.authService.register(email, password, name);
-      await this.userService.createProfile({ name, surname, birth_date, notifications }).toPromise();
       await this.router.navigateByUrl('/home');
     } catch (error) {
       this.errorMessage.set(getAuthErrorMessage(error));
